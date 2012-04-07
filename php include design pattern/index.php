3d857513ff02288ed467f32f9df786737aa0ffea
aligh <aghasizadeh70@gmail.com>
2012-04-07 23:19:28 +0430

    fix some bugs :D

--- php include design pattern/index.php
+++ php include design pattern/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <?php
-	include('inc/pages_head.php');
+	require('inc/pages_head.php'); //age in part ejra nashe complete page mirize beham va chon user natarse REQUIRE beytare :D
 ?>
 </head>
 <body>
@@ -12,7 +12,7 @@
   ?>
   <div class="sidebar1">
     <?php
-		require('inc/menu.php');
+		include('inc/menu.php'); //vas menu bayad INCLUDE bashe :D
 	?>	
     <p> The above links demonstrate a basic navigational structure using an unordered list styled with CSS. Use this as a starting point and modify the properties to produce your own unique look. If you require flyout menus, create your own using a Spry menu, a menu widget from Adobe's Exchange or a variety of other javascript or CSS solutions.</p>
     <p>If you would like the navigation along the top, simply move the ul.nav to the top of the page and recreate the styling.</p>
