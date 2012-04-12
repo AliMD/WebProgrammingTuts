72ab45387ff066b9275df8dee67ab17ee249a556
ali khabbaz <ali.khabbaz14@gmail.com>
2012-04-12 06:04:38 +0430

    change pictres with just one click

--- banner basic/banner.js
+++ banner basic/banner.js
@@ -3,11 +3,22 @@ window.onload=function(){
 	
 	var bannerWidth = 980;
 	
+		
 	var inx=document.getElementsByClassName('banner').item(0).getElementsByClassName('index');
 	
 	var divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
 	
 	var liBtns=document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
+	
+	
+	var j=1;
+	divScrl.onclick=function(){
+		inx.item(0).innerHTML=(j+1)+'/'+5;
+		this.style.left=-(j*980)+'px';
+		
+		j++;
+		if(j==liBtns.length) j=0;  //change pictres with just one click
+	}
 	
 	for(var i=0;i<liBtns.length;i++){
 		(function(i){
