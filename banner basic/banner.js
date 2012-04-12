e238917e5b9566fc2df2a50c145e05d130ce9d26
ali khabbaz <ali.khabbaz14@gmail.com>
2012-04-12 05:53:05 +0430

    number counter  &  fix margin-top bug

--- banner basic/banner.js
+++ banner basic/banner.js
@@ -3,6 +3,8 @@ window.onload=function(){
 	
 	var bannerWidth = 980;
 	
+	var inx=document.getElementsByClassName('banner').item(0).getElementsByClassName('index');
+	
 	var divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
 	
 	var liBtns=document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
@@ -11,6 +13,8 @@ window.onload=function(){
 		(function(i){
 			liBtns.item(i).onclick=function(){
 				divScrl.style.left= (-i*bannerWidth) + 'px';
+				
+				inx.item(0).innerHTML=(i+1)+'/'+liBtns.length;
 			}
 		})(i);
 	}
