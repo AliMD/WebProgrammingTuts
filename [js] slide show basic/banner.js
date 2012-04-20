2df7068beed0e76adbef56da4715e3fa4fdc8489
SalmanM <goldgoldy276@gmail.com>
2012-04-20 23:03:38 +0430

    ghabeliyat haye play, pause, play/pause  ezafe shod !

--- [js] slide show basic/banner.js
+++ [js] slide show basic/banner.js
@@ -1,30 +1,25 @@
 var picWidth = 980,
 currentSlide = 0,
-divScrl, liBtns, picsLen, go2slide, nextSlide, prvSlide;
-
+divScrl, liBtns, picsLen, go2slide, nextSlide, prvSlide,sw=null,play,pause,pp;
 window.onload=function(){
-	
 	divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
-	
 	liBtns = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
-	
+	play	= document.getElementById("play");
+	pause	= document.getElementById("pause");
+	pp		= document.getElementById("pp");//<!-- mean : play/pause button-->
 	picsLen = liBtns.length;
-
 	go2slide = function (n){
 		if(n>=picsLen) n=0;
 		if(n<0) n=picsLen-1;
-		
 		divScrl.style.left = -n*picWidth + 'px';
 		currentSlide=n;
 	};
-	
 	nextSlide = function (){
 		go2slide(currentSlide+1)
 	};
 	prvSlide = function (){
 		go2slide(currentSlide-1)
 	};
-
 	for(var i=0;i<picsLen;i++){
 		(function(j){
 			liBtns.item(j).onclick=function(){
@@ -32,5 +27,19 @@ window.onload=function(){
 			}
 		})(i);
 	}
-	
+	play.onclick=function (){
+		if (sw==null)//<!-- bara in ke har bar ke setinterval mishe speed ziyad nashe !;)  --!>
+		sw = setInterval(nextSlide,1000);
+	}
+	pause.onclick = function (){
+		clearInterval(sw);
+		sw=null;//<!-- baraye control setInterval -->
+	}
+	pp.onclick = function (){
+		if (sw!=null){
+			clearInterval(sw);
+			sw=null;
+		}else
+			sw = setInterval(nextSlide,1000);
+	}
 }
